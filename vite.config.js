c71c9debb6c45f07f1300de168035687f7bfb01f
mireiaacss <mireia.calvino@alumni.esade.edu>
2026-06-15 18:13:11 +0200

    Arreglo deployment

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,5 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/fit-track-app/', 
+  base: 'https://github.com/mireiaacss/fit-track-app', 
 })
